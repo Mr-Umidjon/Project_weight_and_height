cc3acc6d5100dc8b5efa76df84ce2348d7d74314
Umidjon <mrumidjon2020@gmail.com>
2022-05-19 16:25:18 +0500

    plot data

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,7 @@ from read_data import read_csv_data
 from get_data import get_data
 import matplotlib.pyplot as plt
 
+
 def show(gender, weight, height):
     """
     Show data.
@@ -16,12 +17,21 @@ def show(gender, weight, height):
     w_female = []
     h_male = []
     h_female = []
-    
-    #WRITE YOUR CODE HERE
 
+    # WRITE YOUR CODE HERE
+    for i, v in enumerate(gender):
+        if v:
+            w_female.append(weight[i])
+            h_female.append(height[i])
+        else:
+            w_male.append(weight[i])
+            h_male.append(height[i])
+    plt.scatter(h_female, w_female, c='red', s=50, alpha=0.5, label="Female")
+    plt.scatter(h_male, w_male, c='black', s=50, alpha=0.5, label='Male')
+    plt.legend(ncol=2)
     plt.show()
+
 
 data = read_csv_data('data/weight-height.csv')
 gender, weight, height = get_data(data)
-
 show(gender, weight, height)
